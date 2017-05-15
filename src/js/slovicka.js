b59ece61df33bcfa09d145f4b3ba382964cf7f30
Petr <petr@kle.cz>
2017-05-15 19:00:29 +0200

    Změna slovíčka po kliknutí

--- src/js/slovicka.js
+++ src/js/slovicka.js
@@ -1,8 +1,10 @@
 var classname = document.getElementsByClassName("slovicko");
 
 var myFunction = function() {
-    var attribute = this.getAttribute("data-en");
-    alert(attribute);
+    var translation = this.getAttribute("data-translation");
+		var text = this.textContent;
+		this.textContent = translation;
+		this.setAttribute("data-translation", text);
 };
 
 for (var i = 0; i < classname.length; i++) {
