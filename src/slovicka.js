c117b67d6436ee4ce114e5c9b852f5b9ada203da
Petr <petr@kle.cz>
2017-05-17 18:07:19 +0200

    Zkrácení identifikátorů

--- src/slovicka.js
+++ src/slovicka.js
@@ -1,14 +1,14 @@
-var classname = document.getElementsByClassName("flex-item-slovicko");
+var classname = document.getElementsByClassName("fis");
 
 var preklad = function() {
     var translation = this.getAttribute("data-translation");
 		var text = this.textContent;
 		this.textContent = translation;
 		this.setAttribute("data-translation", text);
-		if (this.className === "flex-item-slovicko") {
-			this.className = "flex-item-slovicko-vybrane";
+		if (this.className === "fis") {
+			this.className = "fis-vybrane";
 		}else{
-			this.className = "flex-item-slovicko";
+			this.className = "fis";
 		}
 };
 
